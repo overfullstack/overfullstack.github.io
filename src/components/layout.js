115022a7915578afc60de862b8a9d04f48e30387
Gopal <gakshintala@salesforce.com>
2018-11-28 22:05:13 +0530

    Increased Max Page width

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,7 +57,7 @@ class Template extends React.Component {
         style={{
           marginLeft: 'auto',
           marginRight: 'auto',
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(30),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
