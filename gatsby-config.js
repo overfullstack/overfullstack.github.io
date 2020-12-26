a77c94d3400c2277c82a67b2812916a03dedb4c9
Gopal S Akshintala <gopalsa@microsoft.com>
2020-12-26 13:45:19 +0530

    CNAME plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,61 @@
-const lost = require("lost")
-const pxtorem = require("postcss-pxtorem")
+const lost = require(`lost`)
+const pxtorem = require(`postcss-pxtorem`)
 
 module.exports = {
   siteMetadata: {
-    url: "https://github.com/overfullstack/overfullstack.github.io",
-    siteUrl: "https://overfullstack.ga",
-    blogTitle: "Overfullstack",
-    subtitle:
-      "So excited about this Craft that, I can't keep myself from blogging about it.",
-    caption: "It's All Over full-stack",
-    copyright: "© All rights reserved.",
-    declaration: "I built this blog with React.js",
-    disqusShortname: "gopal",
-    pathPrefix: "/overfullstack.ga",
-    logo: "src/assets/logo.png",
+    url: `https://github.com/overfullstack/overfullstack.github.io`,
+    siteUrl: `https://overfullstack.ga`,
+    blogTitle: `Overfullstack`,
+    subtitle: `So excited about this Craft that, I can't keep myself from blogging about it.`,
+    caption: `It's All Over full-stack`,
+    copyright: `© All rights reserved.`,
+    declaration: `I built this blog with React.js`,
+    disqusShortname: `gopal`,
+    pathPrefix: ``,
+    logo: `src/assets/logo.png`,
     menu: [
       {
-        label: "Articles",
-        path: "/",
+        label: `Articles`,
+        path: `/`,
       },
       {
-        label: "About me",
-        path: "/about-me/",
+        label: `About me`,
+        path: `/about-me/`,
       },
       {
-        label: "My Talks 🎤",
-        path: "/my-talks/",
+        label: `My Talks 🎤`,
+        path: `/my-talks/`,
       },
       {
-        label: "Contact me",
-        path: "/contact-me/",
+        label: `Contact me`,
+        path: `/contact-me/`,
       },
     ],
     author: {
-      name: "Gopal S Akshintala",
-      email: "gopal.akshintala@gmail.com",
-      telegram: "https://t.me/gopalakshintala",
-      twitter: "http://bit.ly/agstwtr",
-      github: "http://bit.ly/agsgithubp",
-      resume: "http://bit.ly/ags-my-resume",
-      linkedin: "http://bit.ly/agslnkd",
-      stackoverflow: "http://bit.ly/agsso",
-      aboutme: "/about-me",
+      name: `Gopal S Akshintala`,
+      email: `gopal.akshintala@gmail.com`,
+      telegram: `https://t.me/gopalakshintala`,
+      twitter: `http://bit.ly/agstwtr`,
+      github: `http://bit.ly/agsgithubp`,
+      resume: `http://bit.ly/ags-my-resume`,
+      linkedin: `http://bit.ly/agslnkd`,
+      stackoverflow: `http://bit.ly/agsso`,
+      aboutme: `/about-me`,
     },
     social: {
-      twitter: "@GopalAkshintala",
+      twitter: `@GopalAkshintala`,
     },
   },
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
+        name: `pages`,
       },
     },
     {
-      resolve: "gatsby-plugin-feed",
+      resolve: `gatsby-plugin-feed`,
       options: {
         query: `
           {
@@ -107,14 +106,14 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title: "Overfullstack's RSS",
+            output: `/rss.xml`,
+            title: `Overfullstack's RSS`,
           },
         ],
       },
     },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
@@ -124,7 +123,7 @@ module.exports = {
             },
           },
           {
-            resolve: "gatsby-remark-embed-video",
+            resolve: `gatsby-remark-embed-video`,
             options: {
               width: 640,
               ratio: 1.77,
@@ -133,58 +132,57 @@ module.exports = {
             },
           },
           {
-            resolve: "gatsby-remark-embed-markdown",
+            resolve: `gatsby-remark-embed-markdown`,
             options: {
               directory: `${__dirname}/src/pages/`,
             },
           },
           {
-            resolve: "gatsby-remark-images",
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 640,
             },
           },
-          "gatsby-remark-code-titles",
+          `gatsby-remark-code-titles`,
           {
-            resolve: "gatsby-remark-prismjs",
+            resolve: `gatsby-remark-prismjs`,
             options: {
-              inlineCodeMarker: "~",
+              inlineCodeMarker: `~`,
               showLineNumbers: true,
             },
           },
           {
-            resolve: "gatsby-remark-autolink-headers",
+            resolve: `gatsby-remark-autolink-headers`,
             options: {
-              offsetY: "100",
-              className: "anchor",
+              offsetY: `100`,
+              className: `anchor`,
               maintainCase: true,
               removeAccents: true,
               isIconAfterHeader: true,
             },
           },
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
           {
-            resolve: "gatsby-remark-external-links",
+            resolve: `gatsby-remark-external-links`,
             options: {
-              target: "_blank",
-              rel: "noopener noreferrer",
+              target: `_blank`,
+              rel: `noopener noreferrer`,
             },
           },
           {
-            resolve: "gatsby-plugin-mailchimp",
+            resolve: `gatsby-plugin-mailchimp`,
             options: {
-              endpoint:
-                "https://github.us7.list-manage.com/subscribe/post?u=ab6b858fe942240463c3a5ab5&amp;id=8851ab5056",
+              endpoint: `https://github.us7.list-manage.com/subscribe/post?u=ab6b858fe942240463c3a5ab5&amp;id=8851ab5056`,
             },
           },
           {
-            resolve: "gatsby-remark-embed-gist",
+            resolve: `gatsby-remark-embed-gist`,
             options: {
               // Optional:
 
               // the github handler whose gists are to be accessed
-              username: "overfullstack",
+              username: `overfullstack`,
 
               // a flag indicating whether the github default gist css should be included or not
               // default: true
@@ -194,16 +192,16 @@ module.exports = {
         ],
       },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: "gatsby-plugin-google-fonts",
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: ["roboto:400,400i,500,700"],
+        fonts: [`roboto:400,400i,500,700`],
       },
     },
     {
-      resolve: "gatsby-plugin-sitemap",
+      resolve: `gatsby-plugin-sitemap`,
       options: {
         query: `
             {
@@ -224,22 +222,22 @@ module.exports = {
                 }
               }
           }`,
-        output: "/sitemap.xml",
+        output: `/sitemap.xml`,
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map((edge) => {
             return {
               url: site.siteMetadata.siteUrl + edge.node.path,
-              changefreq: "daily",
+              changefreq: `daily`,
               priority: 0.7,
             }
           }),
       },
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [
           lost(),
@@ -247,23 +245,23 @@ module.exports = {
             rootValue: 16,
             unitPrecision: 5,
             propList: [
-              "font",
-              "font-size",
-              "line-height",
-              "letter-spacing",
-              "margin",
-              "margin-top",
-              "margin-left",
-              "margin-bottom",
-              "margin-right",
-              "padding",
-              "padding-top",
-              "padding-left",
-              "padding-bottom",
-              "padding-right",
-              "border-radius",
-              "width",
-              "max-width",
+              `font`,
+              `font-size`,
+              `line-height`,
+              `letter-spacing`,
+              `margin`,
+              `margin-top`,
+              `margin-left`,
+              `margin-bottom`,
+              `margin-right`,
+              `padding`,
+              `padding-top`,
+              `padding-left`,
+              `padding-bottom`,
+              `padding-right`,
+              `border-radius`,
+              `width`,
+              `max-width`,
             ],
             selectorBlackList: [],
             replace: true,
@@ -275,24 +273,26 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Gopal's Over full-stack",
-        short_name: "Gopal's Over full-stack",
-        start_url: "/",
-        background_color: "#ffffff",
-        theme_color: "#663399",
-        display: "minimal-ui",
-        icon: "src/assets/favicon.png",
+        name: `Gopal's Over full-stack`,
+        short_name: `Gopal's Over full-stack`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/favicon.png`,
         theme_color_in_head: false,
       },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-131255680-1",
+        trackingId: `UA-131255680-1`,
         head: true,
       },
     },
+    `gatsby-plugin-cname`,
+    `gatsby-plugin-catch-links`,
   ],
 }
