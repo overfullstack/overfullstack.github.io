2176706d85dc7a8c4c6bc22e8babda7cc026ef15
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2021-07-07 19:03:37 +0530

    ncu -u
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,3 @@
-import "applause-button/dist/applause-button"
-import "applause-button/dist/applause-button.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "firebase/firestore"
 
