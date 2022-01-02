5ed3f978fd099fead11e8c775ff5b81bb1ade106
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2022-01-02 12:57:02 +0530

    eslint fix
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ .babelrc.js
@@ -0,0 +1,7 @@
+const ignore = [`**/dist`]
+
+module.exports = {
+  sourceMaps: true,
+  presets: [`babel-preset-gatsby-package`],
+  ignore,
+}
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-const TSEslint = require(`@typescript-eslint/eslint-plugin`)
-
 module.exports = {
-  parser: `babel-eslint`,
+  parser: `@babel/eslint-parser`,
   extends: [
     `google`,
     `eslint:recommended`,
@@ -9,23 +7,20 @@ module.exports = {
     `plugin:react/recommended`,
     `prettier`,
   ],
-  plugins: [
-    `flowtype`,
-    `prettier`,
-    `react`,
-    `filenames`,
-    `simple-import-sort`,
-    `babel`,
-  ],
+  plugins: [`flowtype`, `prettier`, `react`, `filenames`, `@babel`],
   parserOptions: {
+    ecmaVersion: 2016,
     sourceType: `module`,
     ecmaFeatures: {
       jsx: true,
     },
+    babelOptions: {
+      configFile: `./.babelrc.js`,
+    },
   },
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
     node: true,
     jest: true,
   },
@@ -33,20 +28,39 @@ module.exports = {
     before: true,
     after: true,
     spyOn: true,
+    // These should be in scope but for some reason eslint can't see them
+    NodeJS: true,
+    JSX: true,
+    NodeRequire: true,
+    TimerHandler: true,
     __PATH_PREFIX__: true,
     __BASE_PATH__: true,
     __ASSET_PREFIX__: true,
+    _CFLAGS_: true,
+    __GATSBY: true,
   },
   rules: {
+    "@babel/no-unused-expressions": [
+      `error`,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
+    "no-unused-expressions": `off`,
+    "@babel/no-invalid-this": `error`,
+    "no-invalid-this": `off`,
     "arrow-body-style": [
       `error`,
       `as-needed`,
       { requireReturnForObjectLiteral: true },
     ],
-    "no-unused-expressions": [
-      `error`,
+    "new-cap": `off`,
+    "no-unused-vars": [
+      `warn`,
       {
-        allowTaggedTemplates: true,
+        varsIgnorePattern: `^_`,
+        argsIgnorePattern: `^_`,
+        ignoreRestSiblings: true,
       },
     ],
     "consistent-return": [`error`],
@@ -61,7 +75,22 @@ module.exports = {
     "react/prop-types": `off`,
     "require-jsdoc": `off`,
     "valid-jsdoc": `off`,
-    "simple-import-sort/imports": `error`,
+    "prefer-promise-reject-errors": `warn`,
+    "no-prototype-builtins": `warn`,
+    "guard-for-in": `warn`,
+    "spaced-comment": [
+      `error`,
+      `always`,
+      { markers: [`/`], exceptions: [`*`, `+`] },
+    ],
+    camelcase: [
+      `error`,
+      {
+        properties: `never`,
+        ignoreDestructuring: true,
+        allow: [`^unstable_`],
+      },
+    ],
   },
   overrides: [
     {
@@ -88,14 +117,105 @@ module.exports = {
       files: [`*.ts`, `*.tsx`],
       parser: `@typescript-eslint/parser`,
       plugins: [`@typescript-eslint/eslint-plugin`],
+      extends: [`plugin:@typescript-eslint/recommended`],
       rules: {
-        ...TSEslint.configs.recommended.rules,
+        // We should absolutely avoid using ts-ignore, but it's not always possible.
+        // particular when a dependencies types are incorrect.
+        "@typescript-eslint/ban-ts-comment": [
+          `warn`,
+          { "ts-ignore": `allow-with-description` },
+        ],
+        // This rule is great. It helps us not throw on types for areas that are
+        // easily inferrable. However we have a desire to have all function inputs
+        // and outputs declaratively typed. So this let's us ignore the parameters
+        // inferrable lint.
+        "@typescript-eslint/no-inferrable-types": [
+          `error`,
+          { ignoreParameters: true },
+        ],
+        "@typescript-eslint/ban-types": [
+          `error`,
+          {
+            extendDefaults: true,
+            types: {
+              "{}": {
+                fixWith: `Record<string, unknown>`,
+              },
+              object: {
+                fixWith: `Record<string, unknown>`,
+              },
+            },
+          },
+        ],
+        camelcase: `off`,
+        // TODO: These rules allow a lot of stuff and don't really enforce. If we want to apply our styleguide, we'd need to fix a lot of stuff
+        "@typescript-eslint/naming-convention": [
+          `error`,
+          {
+            selector: `default`,
+            format: [`camelCase`],
+          },
+          {
+            selector: `variable`,
+            format: [`camelCase`, `UPPER_CASE`, `PascalCase`],
+            leadingUnderscore: `allowSingleOrDouble`,
+            trailingUnderscore: `allowSingleOrDouble`,
+          },
+          {
+            selector: `function`,
+            format: [`camelCase`, `PascalCase`],
+            leadingUnderscore: `allow`,
+          },
+          {
+            selector: `parameter`,
+            format: [`camelCase`, `PascalCase`, `snake_case`],
+            leadingUnderscore: `allowSingleOrDouble`,
+          },
+          {
+            selector: `enumMember`,
+            format: [`camelCase`, `UPPER_CASE`, `PascalCase`],
+          },
+          {
+            selector: `typeLike`,
+            format: [`PascalCase`],
+          },
+          {
+            selector: `typeAlias`,
+            format: [`camelCase`, `PascalCase`],
+          },
+          {
+            selector: `property`,
+            format: [`PascalCase`, `UPPER_CASE`, `camelCase`, `snake_case`],
+            leadingUnderscore: `allowSingleOrDouble`,
+          },
+          {
+            selector: `objectLiteralProperty`,
+            format: [`PascalCase`, `UPPER_CASE`, `camelCase`, `snake_case`],
+            leadingUnderscore: `allowSingleOrDouble`,
+            trailingUnderscore: `allowSingleOrDouble`,
+          },
+          {
+            selector: `enum`,
+            format: [`PascalCase`, `UPPER_CASE`],
+          },
+          {
+            selector: `method`,
+            format: [`PascalCase`, `camelCase`],
+            leadingUnderscore: `allowSingleOrDouble`,
+          },
+          {
+            selector: `interface`,
+            format: [`PascalCase`],
+            prefix: [`I`],
+          },
+        ],
         // This rule tries to prevent using `require()`. However in node code,
         // there are times where this makes sense. And it specifically is causing
         // problems in our tests where we often want this functionality for module
-        // mocking. At this point it's easier to have it off and just encouarge
+        // mocking. At this point it's easier to have it off and just encourage
         // using top-level imports via code reviews.
         "@typescript-eslint/no-var-requires": `off`,
+        "@typescript-eslint/no-extra-semi": `off`,
         // This rule ensures that typescript types do not have semicolons
         // at the end of their lines, since our prettier setup is to have no semicolons
         // e.g.,
@@ -111,18 +231,11 @@ module.exports = {
             },
           },
         ],
-        // This ensures all interfaces are named with an I as a prefix
-        // e.g.,
-        // interface IFoo {}
-        "@typescript-eslint/interface-name-prefix": [
-          `error`,
-          { prefixWithI: `always` },
-        ],
         "@typescript-eslint/no-empty-function": `off`,
         // This ensures that we always type the return type of functions
         // a high level focus of our TS setup is typing fn inputs and outputs.
         "@typescript-eslint/explicit-function-return-type": `error`,
-        // This forces us to use interfaces over types aliases for object defintions.
+        // This forces us to use interfaces over types aliases for object definitions.
         // Type is still useful for opaque types
         // e.g.,
         // type UUID = string
@@ -148,12 +261,13 @@ module.exports = {
         // bump to @typescript-eslint/parser started showing Flow related errors in ts(x) files
         // so disabling them in .ts(x) files
         "flowtype/no-types-missing-file-annotation": `off`,
+        "@typescript-eslint/array-type": [`error`, { default: `generic` }],
       },
     },
   ],
   settings: {
     react: {
-      version: `16.4.2`,
+      version: `16.9.0`,
     },
   },
 }
