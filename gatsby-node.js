a4a220507af29b3067467c08f95c2d5458079da1
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-03-11 22:57:52 +0530

    Revert "Fix build"
    This reverts commit 55621826ec21510537fe4e961c68bc5c71255bb2.

--- gatsby-node.js
+++ gatsby-node.js
@@ -154,18 +154,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     }
   }
 }
-
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === `build-html`) {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat((context, request, callback) => {
-        const regex = /^@?firebase(\/(.+))?/
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, `commonjs ${request}`) // <- use commonjs!
-        }
-        callback()
-      }),
-    })
-  }
-}
